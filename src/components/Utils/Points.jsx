11c32fbc63e4ddbe4f38e74ed1708915cad39ebe
nipaaa <akternipa941@gmail.com>
2023-08-18 16:55:29 +0600

    update

--- src/components/Utils/Points.jsx
+++ src/components/Utils/Points.jsx
@@ -6,7 +6,7 @@ const Points = ({ options }) => {
       {options?.map((option, index) => (
         <div key={index} className="col-12 col-lg-4 p-1">
           <div className="bookmarkInner">
-            <span className="mb-1">Point {index}</span>
+            <span className="mb-1">Point {index + 1}</span>
             <article>
               <span className="mb-1">{option.title}</span>
               <p>{option.description}</p>
