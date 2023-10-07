4d944c00e83a2c756927b01f73c176644c583fa4
tonni-akter-dev <tanniakhter123@gmail.com>
2023-10-07 18:29:27 +0600

    chat timing added

--- src/components/AuthorChat/Conversation.jsx
+++ src/components/AuthorChat/Conversation.jsx
@@ -6,19 +6,42 @@ import attach from "../../assets/images/attachFile.png";
 import submit from "../../assets/images/submit.png";
 import "./AuthorChat.css";
 const Conversation = () => {
-  const initialState = [
-    { type: "sender", content: [] },
-    { type: "receiver", content: ["i got your message"] },
-  ];
-  const [chatData, setChatData] = useState(initialState);
+  const getCurrentTime = () => {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const ampm = hours >= 12 ? "pm" : "am";
+    const formattedHours = hours % 12 || 12;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    return `${formattedHours}:${formattedMinutes}${ampm}`;
+  };
+
+  const [chatData, setChatData] = useState([
+    { type: "sender", content: "HI", timestamp: getCurrentTime() },
+    {
+      type: "receiver",
+      content: "i got your message",
+      timestamp: getCurrentTime(),
+    },
+  ]);
   const [message, setMessage] = useState("");
+
   const handleInputChange = (e) => {
     setMessage(e.target.value);
   };
+
   const handleSubmit = () => {
     if (message.trim() !== "") {
-      const newMessage = { type: "sender", content: [message] };
-      const newMessageForReceiver = { type: "receiver", content: [message] };
+      const newMessage = {
+        type: "sender",
+        content: message,
+        timestamp: getCurrentTime(),
+      };
+      const newMessageForReceiver = {
+        type: "receiver",
+        content: message,
+        timestamp: getCurrentTime(),
+      };
       setChatData([...chatData, newMessage]);
       setMessage("");
 
@@ -27,6 +50,9 @@ const Conversation = () => {
       }, 2000);
     }
   };
+
+  // Function to get the current time in "6:30pm" format
+
   return (
     <div className="chatList">
       <div className="chatOwner d-flex justify-content-between align-items-center mb-5">
@@ -44,17 +70,17 @@ const Conversation = () => {
       {/* receiver */}
       {chatData.map((data, index) =>
         data.type === "receiver" ? (
-          <div className="grayBox mb-3">
+          <div className="grayBox mb-3" key={index}>
             <div className="whiteBox mb-3">
               <span className="d-block text-dark">you</span>
               <p>{data.content}</p>
             </div>
-            <span className="d-block time">16.00</span>
+            <span className="d-block">{data.timestamp}</span>
           </div>
         ) : (
           <div className="answerBox ml-auto mb-3">
             <p className="mb-2">{data.content} </p>
-            <span className="d-block time">16.00 .Read</span>
+            <span className="d-block time">{data.timestamp} .Read</span>
           </div>
         )
       )}
