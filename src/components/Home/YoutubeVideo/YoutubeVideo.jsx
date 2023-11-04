1876675a06ff4c70c0cc4b1ae379683c4a386d91
muhammadashiqurrahman <getintouch.ashiq@gmail.com>
2023-11-04 10:56:33 +0600

    ::auto play added

--- src/components/Home/YoutubeVideo/YoutubeVideo.jsx
+++ src/components/Home/YoutubeVideo/YoutubeVideo.jsx
@@ -1,3 +1,41 @@
+// import React, { useState } from 'react'
+// import YouTube from 'react-youtube';
+
+// const YoutubeVideo = ({ videoId, thumbnailUrl }) => {
+//     const [videoPlaying, setVideoPlaying] = useState(false);
+
+//     const onReady = (event) => {
+//         event.target.pauseVideo();
+//     };
+
+//     const playVideo = () => {
+//         setVideoPlaying(true);
+//     };
+
+//     const opts = {
+//         height: '390',
+//         width: '640',
+//         playerVars: {
+//             autoplay: 1,
+//         },
+//     };
+//     return (
+//         <div>
+//             {
+//                 !videoPlaying ? (
+//                     <div className="custom-thumbnail" onClick={playVideo}>
+//                         <img src={thumbnailUrl} alt="Custom Thumbnail" />
+//                         <YouTube videoId={videoId} opts={opts} onReady={onReady} />
+//                     </div>
+//                 ) : null
+//             }
+//         </div>
+//     )
+// }
+
+// export default YoutubeVideo
+
+
 import React, { useState } from 'react';
 
 const YoutubeVideo = ({ videoId, thumbnailUrl }) => {
@@ -13,21 +51,23 @@ const YoutubeVideo = ({ videoId, thumbnailUrl }) => {
 
     const opts = {
         height: '350',
+        // width: '640',
         playerVars: {
             autoplay: 1,
         },
     };
 
     return (
-        <div className='video_modal'>
+        <div>
             {!videoPlaying ? (
-                <div className="custom-thumbnail" onClick={playVideo} style={{ cursor: 'pointer' }}>
+                <div className="custom-thumbnail" onClick={playVideo}>
                     <img className='img-fluid' src={thumbnailUrl} alt="Custom Thumbnail" />
                 </div>
             ) : (
                 <div className="youtube-video">
 
                     <iframe
+                        allow='autoplay'
                         title="YouTube Video"
                         width={opts.width}
                         height={opts.height}
