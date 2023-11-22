8a67dab30e0efa9d7bd934eed6bb6be5c3f70478
MamtazDev <akter.mstmamtaz@gmail.com>
2023-11-22 11:03:20 +0600

    added notes annotation on pdf

--- src/components/ReadBook/ReadBooks2.jsx
+++ src/components/ReadBook/ReadBooks2.jsx
@@ -1,65 +1,10 @@
-// import PSPDFKit from "pspdfkit";
-// import { useEffect } from "react";
-// import pdfBooks from '../../assets/book/book.pdf'
-// // G:\FREELANCE PROJECT\OTHERS\devidlory\christianity-book-react\node_modules\pspdfkit\dist
-// // We need to inform PSPDFKit where to look for its library assets, i.e. the location of the `pspdfkit-lib` directory.
-// const baseUrl = `${window.location.protocol}//${window.location.host}/node_modules/pspdfkit/dist/`;
-
-
-// export default function ReadBooks2() {
-//   // Load the PDF when the component mounts
-//   useEffect(() => {
-//     console.log('base',baseUrl)
-
-//     PSPDFKit.load({
-//       baseUrl,
-//       disableWebAssemblyStreaming: true,
-//       container: "#pspdfkit",
-//       document: pdfBooks,
-      
-//     })
-//     .then(instance => {
-//       instance.setViewState(viewState => {
-//         return (
-//              viewState.set("sidebarMode", PSPDFKit.SidebarMode.THUMBNAILS)
-//         )
-//       });
-     
-//       console.log("PSPDFKit loaded", instance);
-//     })
-//     .catch(error => {
-//       console.error(error.message);
-//     });
-      
-//     // PSPDFKit.load({
-//     //   container: "#your-container-id",
-//     //   document: pdfBooks,
-//     //   // other configuration options
-//     // })
-//     // .then(instance => {
-//     //   console.log("PSPDFKit loaded", instance);
-//     // })
-//     // .catch(error => {
-//     //   console.error(error.message);
-//     // });
-
-//     // Cleanup
-//     // return () => PSPDFKit.unload("#your-container-id");
-//   }, []);
-
-//     return <div id="pspdfkit" style={{ width: "100%", height: "100vh" }} />;
-//   }
-  
-
-
-
 import React, { useEffect, useRef } from "react";
 import PSPDFKit from "pspdfkit";
 import pdfBooks from "../../assets/book/book.pdf";
 
 const baseUrl = `${window.location.protocol}//${window.location.host}/node_modules/pspdfkit/dist/`;
 
-export default function ReadBooks2({url}) {
+export default function ReadBooks2({ url }) {
   const instanceRef = useRef(null);
 
   useEffect(() => {
@@ -91,15 +36,31 @@ export default function ReadBooks2({url}) {
     return () => PSPDFKit.unload("#pspdfkit");
   }, []);
 
+  const savePdfToServer = async () => {
+    try {
+      // Capture PDF data from the PSPDFKit instance
+      const pdfData = await instanceRef.current.exportPDF();
 
-  // const arrayBuffer = await instance.exportPDF();
-  // const blob = new Blob([arrayBuffer], { type: 'application/pdf' });
-  // const formData = new FormData();
-  // formData.append("file", blob);
-  // await fetch("/upload", {
-  //   method: "POST",
-  //   body: formData
-  // });
+      console.log("pdfData", pdfData)
+
+      // Send PDF data to the server
+      const response = await fetch("https://your-server-endpoint.com/save-pdf", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/pdf",
+        },
+        body: pdfData,
+      });
+
+      if (response.ok) {
+        console.log("PDF saved successfully.");
+      } else {
+        console.error("Failed to save PDF:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error exporting or saving PDF:", error);
+    }
+  };
 
   const handleAnnotationsChange = async () => {
     try {
@@ -110,7 +71,10 @@ export default function ReadBooks2({url}) {
       const annotations = await instanceRef.current.getAnnotations(pageIndex);
 
       // Log annotations in JSON format
-      console.log("Changes from PDF: ",JSON.stringify(annotations, null, 2));
+      console.log("Changes from PDF: ", JSON.stringify(annotations, null, 2));
+
+      // Save the PDF to the server when annotations change
+      savePdfToServer();
     } catch (error) {
       console.error("Error fetching annotations:", error);
     }
