11ae0c4dc821a4b51416ca989f57e5d37c304f84
mohi14 <mohiuddinkhan1409@gmail.com>
2023-11-23 18:27:10 +0600

    some error fixing on read now

--- src/components/ReadBook/ReadBooks2.jsx
+++ src/components/ReadBook/ReadBooks2.jsx
@@ -12,70 +12,81 @@ export default function ReadBooks2({ url, setArrayBuffer }) {
       try {
         const { annotation } = event;
 
-        console.log('e-anotation',annotation)
-  
+        console.log("e-anotation", annotation);
+
         if (event && event.annotations) {
           const { annotations } = event;
-  
+
           // Process each annotation
           annotations.forEach((annotation) => {
             if (annotation instanceof PSPDFKit.Annotations.TextAnnotation) {
               console.log("Note added:", annotation);
-            } else if (annotation instanceof PSPDFKit.Annotations.HighlightAnnotation) {
+            } else if (
+              annotation instanceof PSPDFKit.Annotations.HighlightAnnotation
+            ) {
               console.log("Highlight added:", annotation);
             }
-          }); }
-          else{
-            console.log("Annotation is not working fine.")
-          }
-  
+          });
+        } else {
+          console.log("Annotation is not working fine.");
+        }
+
         // Save the PDF to the server when annotations change
         savePdfToServer();
       } catch (error) {
         console.error("Error handling annotation added:", error);
       }
     };
-  
+
     PSPDFKit.load({
       baseUrl,
       disableWebAssemblyStreaming: true,
       container: "#pspdfkit1",
-      document: url,
+      document: pdfBooks,
     })
       .then((loadedInstance) => {
         instanceRef.current = loadedInstance;
-  
+
         // Add event listener for annotations change
-        instanceRef.current.addEventListener("annotations.change", handleAnnotationsChange);
-  
+        instanceRef.current.addEventListener(
+          "annotations.change",
+          handleAnnotationsChange
+        );
+
         // Add event listener for annotation added
-        instanceRef.current.addEventListener("annotations.change", handleAnnotationAdded);
-  
+        instanceRef.current.addEventListener(
+          "annotations.change",
+          handleAnnotationAdded
+        );
+
         // Set initial view state (if needed)
         instanceRef.current.setViewState((viewState) => {
           return viewState.set("sidebarMode", PSPDFKit.SidebarMode.THUMBNAILS);
         });
-  
+
         console.log("PSPDFKit loaded", instanceRef.current);
       })
       .catch((error) => {
         console.error(error.message);
       });
-  
+
     // Cleanup
     return () => {
       // Remove the event listeners
       if (instanceRef.current) {
-        instanceRef.current.removeEventListener("annotations.change", handleAnnotationsChange);
-        instanceRef.current.removeEventListener("annotations.change", handleAnnotationAdded);
+        instanceRef.current.removeEventListener(
+          "annotations.change",
+          handleAnnotationsChange
+        );
+        instanceRef.current.removeEventListener(
+          "annotations.change",
+          handleAnnotationAdded
+        );
         PSPDFKit.unload("#pspdfkit1");
         console.log("Cleanup function called");
       }
     };
   }, [url]);
-
-
-
 
   const savePdfToServer = async () => {
     try {
@@ -83,16 +94,19 @@ export default function ReadBooks2({ url, setArrayBuffer }) {
       const pdfData = await instanceRef.current.exportPDF();
 
       // console.log("pdfData", pdfData)
-      setArrayBuffer(pdfData)
+      setArrayBuffer(pdfData);
 
       // Send PDF data to the server
-      const response = await fetch("https://your-server-endpoint.com/save-pdf", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/pdf",
-        },
-        body: pdfData,
-      });
+      const response = await fetch(
+        "https://your-server-endpoint.com/save-pdf",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/pdf",
+          },
+          body: pdfData,
+        }
+      );
 
       if (response.ok) {
         console.log("PDF saved successfully.");
@@ -114,17 +128,11 @@ export default function ReadBooks2({ url, setArrayBuffer }) {
 
       // console.log('annotation', annotations)
 
-
-
-
       // Filter annotations for highlights and notes
       // const highlights = annotations.filter(annotation => annotation.type === "highlight" &&  console.log("Highlights:", highlights));
       // const notes = annotations.filter(annotation => annotation.type === "note" && console.log("Notes:", notes));
 
       // Log highlights and notes in the console
-     
-      
-      
 
       // Log annotations in JSON format
       // console.log("Changes from PDF: ", JSON.stringify(annotations, null, 2));
