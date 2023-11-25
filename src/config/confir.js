90c5fd5401eb22f864119cd50f43cbc4e2c1fbeb
mohi14 <mohiuddinkhan1409@gmail.com>
2023-11-25 10:46:16 +0600

    gg

--- src/config/confir.js
+++ src/config/confir.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 // export const BASE_URL = "https://devid-9ngw.onrender.com";
 export const BASE_URL = "http://localhost:8000";
-=======
-export const BASE_URL = "http://localhost:8000";
-// export const BASE_URL = "https://devid-9ngw.onrender.com";
->>>>>>> 19e50ec16a4d220a616644ad93a4ffd02bdb0e7c
